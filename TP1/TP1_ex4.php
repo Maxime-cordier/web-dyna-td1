4250fce6ca82375d04f37cc29717b15d59536e44
Maxime-cordier <21901772@etu.unicaen.fr>
2020-03-26 15:24:03 +0100

    ex

---
+++ TP1/TP1_ex4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ex 4</title>
+	</head>
+	<body>
+	
+	<table border='1'>
+    <tbody>
+       
+	   <tr>
+			<?php
+			$nbcolonnes=0;
+			while($nbcolonnes<2)
+			{
+				echo '<td>1-0</td>';
+				$nbcolonnes = $nbcolonnes +1;
+			}
+			?>
+        </tr>
+		
+		<?php
+
+
+	</body>
+</html>
