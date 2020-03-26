884cda3b9b98432008f2dd9c64bb03c671467b19
Maxime-cordier <21901772@etu.unicaen.fr>
2020-03-26 15:14:06 +0100

    td1 exo4

---
+++ TD1/TP1_ex4.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ex 4</title>
+	</head>
+	<body>
+	
+	<table border='1'>
+    <tbody>
+       
+	   <tr>
+			<?php
+			$nbcolonnes=0;
+			while($nbcolonnes<$_GET["nbc"])
+			{
+				echo '<td>1-0</td>';
+				$nbcolonnes = $nbcolonnes +1;
+			}
+			?>
+        </tr>
+		
+		<?php
+
+<a href="?l=2&c=2">Tableau 2 x 2</a><br>
+<a href="?l=5&c=5">Tableau 5 x 5</a><br>
+<a href="?l=10&c=8">Tableau 10 x 8</a><br>
+<hr>
+<?php
+//Récupère les paramètres l et c passés dans l'url
+$nbLignes=$_GET["l"]??10;
+$nbColonnes=$_GET["c"]??10;
+echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
+?>
+<table border='1'>
+    <tbody>
+      <?php for($i=0;$i<$nbLignes;$i++){
+        $gras=($i%2==0)?"bold":"normal";
+      ?>
+        <tr>
+        <?php for($j=0;$j<$nbColonnes;$j++){
+            $couleur=($j%2==0)?"red":"black";
+            ?>
+            <td style="font-weight: <?=$gras ?>;color:<?=$couleur ?>"><?="$i - $j"?></td>
+        <?php }?>
+        </tr>
+      <?php }?>
+    </tbody>
+</table>
+
+    
+	</tbody>
+	</table>
+	
+
+
+	</body>
+</html>
